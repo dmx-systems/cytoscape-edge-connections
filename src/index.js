5bc6a9d0ff326d8b427ea6aaaa0e0827b053065e
Jörg Richter <jri@deepamehta.de>
2018-12-30 16:54:55 +0100

    Autoscaffold Cytoscape extension (using slush)

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const impl = require('./core');
+
+// registers the extension on a cytoscape lib ref
+let register = function( cytoscape ){
+  if( !cytoscape ){ return; } // can't register if cytoscape unspecified
+
+  cytoscape( 'core', 'edgeConnections', impl ); // register with cytoscape.js
+};
+
+if( typeof cytoscape !== 'undefined' ){ // expose to global cytoscape (i.e. window.cytoscape)
+  register( cytoscape );
+}
+
+module.exports = register;
